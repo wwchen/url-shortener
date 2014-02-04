41be091a3c9d87260d1a14888995fae11bd5bf05
William Chen <williamchen16@gmail.com>
2014-02-04 22:03:22 +0800

    A wild rails app appears!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shortner::Application.config.secret_key_base = 'ae7f20c0504bb74fa21fac1eb4a3021aebe6953e5e2ae00939d7ec58fa9656b6717ff6785539b205d7f263b97cf7d502a5bd58245b1206259bc3582003d610fe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shortner::Application.config.session_store :cookie_store, key: '_shortner_session'

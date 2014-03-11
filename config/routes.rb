daad6c0ef8e1886cbe00db9691bc4a345c62e1a3
William Chen <williamchen16@gmail.com>
2014-03-11 14:54:07 -0700

    Various fixes, including a migration
    - Reflecting custom field correctly, after an update
    - New field to prevent links from accidental deletions
    - Changed URL routing by id to by alias
    - Modified forms to reflect new fields
    - Hide delete button if 'sticky' is true
    - DB migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 UrlShortener::Application.routes.draw do
-  post 'url' => 'url#index'
-  resources :urls
+  #post 'url' => 'url#index'
+  # http://stackoverflow.com/questions/2634964/how-to-add-a-custom-restful-route-to-a-rails-app
+  resources :urls do
+    collection do
+      get 'splash'
+    end
+  end
 
   get ':alias' => 'urls#redirect'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140205062512) do
+ActiveRecord::Schema.define(version: 20140306020046) do
 
   create_table "urls", force: true do |t|
     t.string   "href"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20140205062512) do
     t.integer  "views",      default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "sticky"
+    t.boolean  "custom"
   end
 
   add_index "urls", ["alias"], name: "index_urls_on_alias", unique: true
